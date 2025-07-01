b890a9c2cd178875f29da9e8150329534ca48259
Kaio Garcia <38477535+qakaio@users.noreply.github.com>
2025-07-01 19:16:06 -0300

    Delete tests/login.spec.ts

--- tests/login.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/LoginPage';
-
-test('Login with valid credentials', async ({ page }) => {
-  const login = new LoginPage(page);
-  await login.goto();
-  await login.login('standard_user', 'secret_sauce');
-  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
-});
