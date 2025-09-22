5d3c638813839d7a24f4d3996e4f972944219147
Kaio Garcia <38477535+qakaio@users.noreply.github.com>
2025-09-22 19:38:33 -0300

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: 2,
+  workers: 1,
   timeout: 60000,
   expect: {
     timeout: 15000
